cf1c45574fe4ef3b5467657e1ec56c1a65cdb6d3
Brbdogsonfire <109932459+Brbdogsonfire@users.noreply.github.com>
2023-09-12 21:56:22 -0700

    Update object_renderer.py

--- object_renderer.py
+++ object_renderer.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from raycasting import *
 from settings import *
 
 
@@ -8,6 +9,14 @@ class ObjectRenderer:
         self.screen = game.screen
         self.wall_textures = self.load_wall_textures()
 
+    def draw(self):
+        self.render_game_object()
+
+    def render_game_object(self):
+        list_objects = self.game.raycasting.objects_to_render
+        for depth, image, pos in list_objects:
+            self.screen.blit(image, pos)
+
     @staticmethod
     def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
         texture = pg.image.load(path).convert_alpha()
@@ -15,4 +24,6 @@ class ObjectRenderer:
 
     def load_wall_textures(self):
         return { 
-            1: self.get_texture('1.png') }
+            1: self.get_texture('1.png'),
+            2: self.get_texture('2.png'),
+            }
